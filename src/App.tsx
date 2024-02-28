a069a8ca19ad886810543bcb3659f0e53e233989
Oldemar <oldemego@gmail.com>
2024-02-28 08:30:42 +0000

    heading created

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="bg-gray-900 min-h-screen text-white">
+      <main className="w-full lg:w-9/12 md:w-10/12 xl:w-8/12 min-h-screen flex mx-auto">
+        <section className="mt-56 font-mono flex flex-col gap-8">
+          <div>
+            <h1 className="text-green-blue font-bold text-xl">Hi, I'm</h1>
+          </div>
+          <div>
+            <h2 className="text-lightest-slate text-7xl font-bold">Oldemar Gonçalves</h2>
+          </div>
+          <div>
+            <h3 className="text-lowest-slate text-4xl font-bold">I build web and native application for joy</h3>
+          </div>
+          <div>
+            <p className="max-w-[540px] text-lowest-slate font-bold text-lg">Currently, I'm software developer, working in system management and automation. I like re-creating beatiful website and other application that I found through out internet.</p>
+          </div>
+          <div className="mt-5">
+
+            <a className="text-green-blue outline outline-2 outline-offset-2 hover:outline-4 p-4 font-bold" href="#" target="_blank">
+              Check out my Linkedin!
+            </a>
+          </div>
+        </section>
+      </main>
+    </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'green-blue': '#64ffda',
+        'lightest-slate': '#ccd6f6',
+        'lowest-slate': '#8892b0'
+      },
+      fontFamily: {
+        'font-mono': ['SF Mono', 'Fira Code', 'Fira Mono', 'Roboto Mono', 'monospace']
+      }
+    },
   },
   plugins: [],
 }
